7b476fade0522457e800bbddade91c734902d83f
Alexey Poltavskiy <79776177342@yandex.ru>
2023-02-23 22:34:20 +0300

    admin interface refinement

--- task2/articles/admin.py
+++ task2/articles/admin.py
@@ -5,14 +5,19 @@ from .models import Article, Scope, Tag
 
 class ScopeInlineFormset(BaseInlineFormSet):
     def clean(self):
+        main_tegs = []
         for form in self.forms:
             # В form.cleaned_data будет словарь с данными
             # каждой отдельной формы, которые вы можете проверить
-            form.cleaned_data
+            main_tegs.append(form.cleaned_data.get('is_main'))
             # вызовом исключения ValidationError можно указать админке о наличие ошибки
             # таким образом объект не будет сохранен,
             # а пользователю выведется соответствующее сообщение об ошибке
-            
+            #raise ValidationError('Тут всегда ошибка')
+        if main_tegs.count(True) > 1:
+            raise ValidationError('только один из тэгов может быть основным.')
+        if main_tegs.count(True) == 0:
+            raise ValidationError('один из тэгов должен быть основным.')
         return super().clean()  # вызываем базовый код переопределяемого метода
 
 
